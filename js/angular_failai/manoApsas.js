91f4986247278d988bbfa438f33cafdb5d01d357
Tautvydas Dulskis <tautvydas@dulskis.eu>
2018-04-17 20:54:04 +0300

    Angular Routing. Htaccess file

--- js/angular_failai/manoApsas.js
+++ js/angular_failai/manoApsas.js
@@ -1,4 +1,4 @@
-var app = angular.module('manoApsas', []);
+var app = angular.module('manoApsas', ["ngRoute"]);
 
 app.controller('menuController', function ($http) {
     let vm = this;
@@ -6,5 +6,18 @@ app.controller('menuController', function ($http) {
     $http.get("/meniu.json")
         .then(function(response) {
             vm.meniu = response.data;
+        });
+});
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "/inc/apie.html"
+        })
+        .when("/zaidimas", {
+            templateUrl : "/inc/zaidimas.html"
+        })
+        .when("/kontaktai", {
+            templateUrl : "/inc/kontaktai.html"
         });
 });
